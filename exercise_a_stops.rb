d05b4b7feff7fb76e2922c2add0f728833cdb530
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-03-11 20:00:12 +0000

    Arrays Exercise A

---
+++ exercise_a_stops.rb
@@ -0,0 +1,37 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+#1. Add "Edinburgh Waverley" to the end of the array
+stops[6] = "Edinburgh Waverley"
+p stops
+
+#2. Add "Glasgow Queen St" to the start of the array
+stops[0] = "Glasgow Queen St"
+p stops
+
+#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops[4] = "Polmont"
+p stops
+
+#4. Print out the index position of "Linlithgow"
+p stops.index("Linlithgow")
+
+#5. Remove "Livingston" from the array using its name
+p stops.delete("Livingston")
+
+#6. Delete "Cumbernauld" from the array by index
+p stops.delete_at(1)
+
+#7. Print the number of stops there are in the array?
+p stops.count
+
+#8. Show as many ways as you can to return "Falkirk High" from the array?
+p stops[1]
+p stops.fetch(1)
+
+#9. Reverse the positions of the stops in the array
+p stops.reverse
+
+#10 Print out all the stops using a for loop
+for stop in stops
+  p stop
+end 
