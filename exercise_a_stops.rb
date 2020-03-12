83be8f6b1d07710ca3d478eaa4b1843d9b537e0f
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-03-12 09:52:31 +0000

    exercise a new edits w/notes

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -3,14 +3,17 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 #1. Add "Edinburgh Waverley" to the end of the array
 stops[6] = "Edinburgh Waverley"
 p stops
+# also use .push
 
 #2. Add "Glasgow Queen St" to the start of the array
 stops[0] = "Glasgow Queen St"
 p stops
+#also use .unshift
 
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops[4] = "Polmont"
 p stops
+#use .insert(index num, "String")
 
 #4. Print out the index position of "Linlithgow"
 p stops.index("Linlithgow")
@@ -23,10 +26,12 @@ p stops.delete_at(1)
 
 #7. Print the number of stops there are in the array?
 p stops.count
+#could also use .size() and .length()
 
 #8. Show as many ways as you can to return "Falkirk High" from the array?
 p stops[1]
 p stops.fetch(1)
+# could also use: .at(index-negative num), .at(index num)
 
 #9. Reverse the positions of the stops in the array
 p stops.reverse
@@ -34,4 +39,4 @@ p stops.reverse
 #10 Print out all the stops using a for loop
 for stop in stops
   p stop
-end 
+end
